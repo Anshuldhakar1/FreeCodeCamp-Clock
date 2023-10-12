ddafc6dafb0a7e23ab90b178716d83aa33460053
Anshul <anshuldhakar03@gmail.com>
2023-10-13 01:17:56 +0530

    initial setup

---
+++ src/app/Main.tsx
@@ -0,0 +1,10 @@
+export const Main = () => { 
+  return (
+    <div id="main">
+      <h2>Hello World!</h2>
+    </div>
+  );
+};
+
+export default Main;
+
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
+import Main from './Main';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="clock" />
+    <div id="Container">
+      <Main />
     </div>
   );
 }
